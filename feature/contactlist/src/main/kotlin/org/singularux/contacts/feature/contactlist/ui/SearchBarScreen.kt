2d166b6b8c21fbf7b159370b16415d9c5687fdc3
xNovo3000 <fontana.ciro.2001@gmail.com>
2024-12-12 23:29:41 +0100

    Added contact item list, removed old search bar screen

--- feature/contactlist/src/main/kotlin/org/singularux/contacts/feature/contactlist/ui/SearchBarScreen.kt
+++
@@ -1,8 +0,0 @@
-package org.singularux.contacts.feature.contactlist.ui
-
-import androidx.fragment.app.Fragment
-import dagger.hilt.android.AndroidEntryPoint
-import org.singularux.contacts.feature.contactlist.R
-
-@AndroidEntryPoint
-class SearchBarScreen : Fragment(R.layout.screen_search_bar)
