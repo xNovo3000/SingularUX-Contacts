54e928b0b791767ae3a402cfae222faa87ff73b4
xNovo3000 <fontana.ciro.2001@gmail.com>
2025-12-31 18:24:30 +0100

    Added first activity with first UI

--- app/src/main/java/org/singularux/contacts/ui/ContactListActivity.java
+++ app/src/main/java/org/singularux/contacts/ui/ContactListActivity.java
@@ -1,9 +1,28 @@
 package org.singularux.contacts.ui;
 
+import android.os.Bundle;
+
 import androidx.activity.ComponentActivity;
+import androidx.annotation.Nullable;
+import androidx.core.splashscreen.SplashScreen;
+import androidx.core.view.WindowCompat;
+
+import org.singularux.contacts.databinding.ActivityContactListBinding;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class ContactListActivity extends ComponentActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        // Start activity with splash screen and edge-to-edge support
+        SplashScreen.installSplashScreen(this);
+        WindowCompat.enableEdgeToEdge(getWindow());
+        super.onCreate(savedInstanceState);
+        // Set content view using view binding
+        ActivityContactListBinding binding = ActivityContactListBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
 }
