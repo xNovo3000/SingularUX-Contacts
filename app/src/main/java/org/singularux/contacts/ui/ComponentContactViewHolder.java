f01183ee135e2dc4d9316d75a4b66ce4b402c7c9
xNovo3000 <fontana.ciro.2001@gmail.com>
2025-12-31 19:21:03 +0100

    Added contact component class and layout

---
+++ app/src/main/java/org/singularux/contacts/ui/ComponentContactViewHolder.java
@@ -0,0 +1,30 @@
+package org.singularux.contacts.ui;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.android.material.textview.MaterialTextView;
+
+import org.singularux.contacts.databinding.ComponentContactBinding;
+
+import lombok.Getter;
+import lombok.val;
+
+@Getter
+public class ComponentContactViewHolder extends RecyclerView.ViewHolder {
+
+    private final ShapeableImageView avatarImage;
+    private final MaterialTextView avatarText, headline;
+
+    public ComponentContactViewHolder(@NonNull View itemView) {
+        super(itemView);
+        val binding = ComponentContactBinding.bind(itemView);
+        this.avatarImage = binding.componentContactAvatarImage;
+        this.avatarText = binding.componentContactAvatarText;
+        this.headline = binding.componentContactHeadline;
+    }
+
+}
